69edc5946d130ebb23d22d7dbc0398874389be18
Marcos Barbero <marcos.hgb@gmail.com>
2016-03-14 11:10:14 -0300

    Add spring.application.name

--- src/main/java/demo/Application.java
+++ src/main/java/demo/Application.java
@@ -11,7 +11,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringCloudApplication
 public class Application {
 
-        public static void main(String... args) {
-            SpringApplication.run(Application.class, args);
-        }
+	public static void main(String... args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+
 }
